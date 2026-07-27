56fd847a9aac66994d467979ab694e71e3170e0f
sahilslomo <sahilslomo@gmail.com>
2026-07-27 21:47:25 +0530

    admin protected

--- lib/firebase-admin.ts
+++ lib/firebase-admin.ts
@@ -1,24 +1,22 @@
 import { cert, getApps, initializeApp } from "firebase-admin/app";
 import { getAuth } from "firebase-admin/auth";
 
-import serviceAccount from "@/config/serviceAccountKey.json";
-
 console.log("🔥 FIREBASE ADMIN FILE LOADED");
-console.log("🔥 SERVICE ACCOUNT:", {
-    project_id: serviceAccount.project_id,
-    client_email: serviceAccount.client_email,
-});
 
 const adminApp =
     getApps().length === 0
         ? initializeApp({
               credential: cert({
-                  projectId: serviceAccount.project_id,
-                  clientEmail: serviceAccount.client_email,
-                  privateKey: serviceAccount.private_key.replace(
-                      /\\n/g,
-                      "\n"
-                  ),
+                  projectId: process.env.FIREBASE_PROJECT_ID,
+
+                  clientEmail:
+                      process.env.FIREBASE_CLIENT_EMAIL,
+
+                  privateKey:
+                      process.env.FIREBASE_PRIVATE_KEY?.replace(
+                          /\\n/g,
+                          "\n"
+                      ),
               }),
           })
         : getApps()[0];
